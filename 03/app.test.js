4c51ae1309b847f7a320b736c0ff2e0fa6f43b27
Agnieszka Kaminska <zabula81@o2.pl>
2024-01-25 12:56:31 +0100

    value in the range from min to max

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,12 +1,20 @@
-import randomNumber from './app';
+import randomNumber from "./app";
 
-test("Equal One", () =>{
-    expect(randomNumber(1,1)).toBe(1)
-})
-
+test("Equal One", () => {
+  expect(randomNumber(1, 1)).toBe(1);
+});
 
 // describe('Equale One', () =>{
 //     it('should input 1', () =>{
 //         expect(randomNumber(1,1)).toBe(1)
 //     })
 // })
+
+test("should return a value in the range from min to max", () => {
+  const min = 1;
+  const max = 10;
+  const value = randomNumber(min, max);
+  expect(value).toBeGreaterThanOrEqual(min);
+  expect(value).toBeLessThan(max);
+});
+
