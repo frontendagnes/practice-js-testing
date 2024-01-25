2e758c2f2765b7e2adfcb5891ee665d07abe7fbb
Agnieszka Kaminska <zabula81@o2.pl>
2024-01-25 13:03:57 +0100

    refactoring func randomNumber

--- 03/app.js
+++ 03/app.js
@@ -1,9 +1,13 @@
 export default function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+   if (min <= max) {
+     return Math.floor(Math.random() * (max - min + 1) + min);
+   } else throw new RangeError();
 }
 
 // function randomNumber(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
+//   if (min < max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   } else throw new RangeError();
 // }
 
-// console.log(randomNumber(1,1));
+console.log(randomNumber(1, 4));
