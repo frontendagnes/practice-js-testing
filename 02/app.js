67a8ae7151b0a4e223a0ef60515941fd183f7011
Agnieszka Kaminska <zabula81@o2.pl>
2024-01-25 12:00:17 +0100

    second

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,43 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector(".error--click");
+  const enterEl = document.querySelector(".error--enter");
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
-
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
-
+  initEventWithError(clickEl, "click", new RangeError("Błąd zakresu!"));
+  initEventWithError(enterEl, "mouseenter", new TypeError("Błąd typu!"));
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+  element.style.top = Math.random() * 600 + "px";
+  element.style.left = Math.random() * 800 + "px";
 
-    if(error) {
-        throw error;
-    }
+  if (error) {
+    throw error;
+  }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  // element.addEventListener(eventName, function () {
+  //   setRandomPosition(this, error);
+  // });
+  element.addEventListener(eventName, function () {
+    try {
+      setRandomPosition(this, error);
+    } catch (error) {
+      showError(error);
+    }
+  });
+}
+
+function showError(error) {
+  const alert = document.querySelector(".alert");
+  alert.classList.remove("alert--hidden");
+  alert.innerText = error;
+
+  alert.addEventListener("click", () => {
+    alert.classList.add("alert--hidden");
+  });
 }
