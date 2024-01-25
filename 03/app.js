428d2dbb2873bcb67a213258ea0fa747612042d8
Agnieszka Kaminska <zabula81@o2.pl>
2024-01-25 12:18:23 +0100

    added code

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,9 @@
 export default function randomNumber(min, max) {
- 
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// function randomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// console.log(randomNumber(1,1));
